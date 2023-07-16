df4524e7aa3ddced3a0753ce932b70b1d066c0c5
MD SAHID AHMED <iamsahidofficial99@gmail.com>
2023-07-16 22:53:41 +0600

    Re Design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,6 +11,7 @@ const Contact = () => {
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116302.8384081535!2d91.46574995374068!3d24.36516746217412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3751684a2e2ae12b%3A0xb02bd1fdff59a650!2sBahubal%20Upazila!5e0!3m2!1sen!2sbd!4v1689511353342!5m2!1sen!2sbd"
         width="100%"
+        title="name"
         height="500"
         style={{ border: 0 }}
         allowFullScreen=""
