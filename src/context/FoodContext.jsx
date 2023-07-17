5ba24e985258006761a9845bb423ba398e4fdf57
MD SAHID AHMED <iamsahidofficial99@gmail.com>
2023-07-17 17:06:30 +0600

    Some Add and modification

--- src/context/FoodContext.jsx
+++ src/context/FoodContext.jsx
@@ -1,8 +1,23 @@
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 const AppContext = createContext();
 
+const API = "http://localhost:3001/foods"
+
 const AppProvier = ({ children }) => {
+
+
+  const getFoods = async (url)=>{
+    const res = await axios.get(url);
+    console.log("🚀 ~file: FoodContext.jsx ~ line 10 ~ getProducts ~ res", res)
+
+  }
+
+    useEffect(()=>{
+      getFoods(API);
+    },[])
+ 
   return (
     <AppContext.Provider value={"sahid ahmed"}>{children}</AppContext.Provider>
   );
